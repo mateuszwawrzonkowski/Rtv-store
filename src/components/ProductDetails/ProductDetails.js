118403927b5925f00134f54684d13643dda59876
mwawrzonkowski <wawko2@gmail.com>
2020-10-12 15:16:59 +0200

    first commit

---
+++ src/components/ProductDetails/ProductDetails.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductDetails = () => (
+  <div>product details</div>
+);
+
+export default ProductDetails;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Cart } from './Cart';
+export { default as Navigation } from './Navigation';
+export { default as ProductDetails } from './ProductDetails';
+export { default as Products } from './Products';
